99828e1f6d50b04cabbe31460277f014981d7397
warezoid <warezoid@proton.me>
2025-11-25 19:29:13 +0100

    [ Version 0.0.0 ] - repo init.

---
+++ servo_driver/servo_driver.ino
@@ -0,0 +1,122 @@
+#include <ESP32Servo.h>
+
+#define VENT_SERVO_LEFT_PIN 26
+#define VENT_SERVO_RIGHT_PIN 25
+#define VENT_SERVO_MIN 0
+#define VENT_SERVO_MAX 80
+
+Servo vent_servo_left, vent_servo_right;
+short vent_servo_pos = VENT_SERVO_MIN;
+
+void move_servo(int pos){
+  vent_servo_left.write(pos);
+  vent_servo_right.write(pos);
+}
+short calc_move(float open_rate){
+  if(open_rate <= 1){
+    if(open_rate >= 0){
+      return (short)(open_rate * VENT_SERVO_MAX);
+    }
+
+    return VENT_SERVO_MIN;
+  }
+
+  return VENT_SERVO_MAX;
+}
+
+
+
+/*
+  - maximalni delka requestu 100 znaku + 0 (ukoncovaci)
+  - command -> prvnich 10 znaku | pole: + 0 ukoncovaci 
+
+  -> SET___MOVE <FLOAT-NUMBER> <COMMAND-TERMINATOR>
+*/
+
+#define max_request_length 101
+#define max_command_length 11
+#define request_terminator '\n'
+#define request_padding '_'
+
+char request[max_request_length];
+char command[max_command_length];
+int request_index = 0;
+
+
+
+void print_buf(char *buf, int len){
+  printf("%s\n", buf);
+  
+  for(int i = 0; i < len; i++){
+    printf("%d ", (unsigned char)buf[i]);
+  }
+
+  printf("\n");
+}
+
+
+
+void get_command(){
+  int i = 0;
+  memset(command, 0, sizeof(command));
+
+  for(int j = 0; j < (max_command_length - 1); j++){
+    if(request[j] != request_padding){
+      command[i] = request[j];
+      i++;
+    }
+  }
+
+  command[max_command_length - 1] = 0;
+}
+void identify_request(){
+  get_command();
+
+  print_buf(command, max_command_length);
+}
+void read_serial(){
+  while(Serial.available()){
+    char c = Serial.read();
+
+    if(c == request_terminator){
+      request[max_request_length - 1] = 0;
+
+      identify_request();
+
+      request_index = 0;
+      memset(request, 0, sizeof(request));
+    }
+    else{
+      if(request_index < max_request_length){
+        request[request_index] = c;
+        request_index++;
+      }
+    }
+  }
+}
+
+
+
+void setup(){
+  vent_servo_left.attach(VENT_SERVO_LEFT_PIN);
+  vent_servo_right.attach(VENT_SERVO_RIGHT_PIN);
+
+  Serial.begin(115200);
+}
+
+/*
+
+  -> servo is lagged in vent_servo_pos by executive unit (EU)
+  -> by sensor unit (SU) is calculated open_rate and is send request to EU to move_servo
+  -> EU catch request call calc_move, which set vent_servo_pos
+  -> servo is lagged... and respond with OK status
+  -> REPEAT
+
+*/
+
+int i = 0;
+
+void loop(){
+  move_servo(vent_servo_pos);
+  read_serial();
+}
